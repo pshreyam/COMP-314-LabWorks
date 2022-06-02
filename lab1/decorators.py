36cd15b01e55dce96f5b109a0aef0039f38b272f
Shreyam Pokharel <pshreyam@gmail.com>
2022-06-02 18:59:57 +0545

    Replace time.time function with time.perf_counter function

--- lab1/decorators.py
+++ lab1/decorators.py
@@ -6,9 +6,9 @@ def timer(f):
     """Return the execution time (in microseconds) along with the result of each function."""
     @wraps(f)
     def wrap(*args, **kwargs):
-        t1 = time.time()
+        t1 = time.perf_counter()
         result = f(*args, **kwargs)
-        t2 = time.time()
+        t2 = time.perf_counter()
         duration = (t2 - t1) * 1000 * 1000
         return {
             "duration": duration,
